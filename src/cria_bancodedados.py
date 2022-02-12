358a867a0c01b22720060eae5d7f7b94976ecce5
JulioGoes <goes.cesar157@gmail.com>
2022-02-12 12:16:36 -0300

    update inicial

--- src/cria_bancodedados.py
+++ src/cria_bancodedados.py
@@ -5,10 +5,11 @@ import pandas as pd
 def cria_tabela_classe(nome):
 
     conn = sqlite3.connect('bancodedados/bancodedados.db')
-
     df = pd.read_csv('arquivos/lv5_' + nome + '_progressao.csv')
-    conn.execute('CREATE TABLE IF NOT EXISTS ' + nome + ' \
-                (Nível text, XP int, DV int, BA int, JP int)')
+    conn.execute(
+        'CREATE TABLE IF NOT EXISTS ' + nome + ' (Nível text, XP int, \
+        DV int, BA int, JP int)'
+    )
     df.to_sql(name=nome, con=conn, if_exists='replace', index=False)
 
     conn.commit()
