7ff2d0ae1d3a3f0a5f54cf2ab0548bfee68fa8af
JulioGoes <goes.cesar157@gmail.com>
2022-02-13 14:23:47 -0300

    update

--- src/consulta_bancodedados.py
+++ src/consulta_bancodedados.py
@@ -1,18 +1,26 @@
 import sqlite3
 
 
-# A função a seguir coleta
+# A função a seguir coleta as informações respectiva de cada classe
+# e retorna uma super lista com os valores
 def cria_lista_classe(classe):
 
     conn = sqlite3.connect('bancodedados/bancodedados.db')
     con = conn.cursor()
 
     lista = []
-    if classe == 'mago' or classe == 'clerigo':
+    if classe == 'mago':
         for row in con.execute('SELECT * FROM ' + classe + ' INNER \
                                 JOIN ' + classe + '_magia ON ' +
                                classe + '.nivel = \
                                 ' + classe + '_magia.nivel'):
+            lista.append(row)
+    elif classe == 'clerigo':
+        for row in con.execute('SELECT * FROM ' + classe + ' INNER \
+            JOIN ' + classe + '_magia ON ' + classe + '.nivel = \
+             ' + classe + '_magia.nivel INNER \
+            JOIN ' + classe + '_afastar_mortos_vivos ON \
+             ' + classe + '.nivel= ' + classe + '_afastar_mortos_vivos.nivel'):
             lista.append(row)
     else:
         for row in con.execute('SELECT * FROM ' + classe + ''):
